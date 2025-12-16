3af6941c1fda5fb27070453698313cdff178a01c
dev1lfreak <kvachuk.sergey@yandex.ru>
2025-12-16 16:38:58 +0300

    fix : added base to vite file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/bg_forum/',
   plugins: [react({
     jsxRuntime: 'automatic',
   })],
