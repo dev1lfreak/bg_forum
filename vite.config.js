84e6460daf6ac198f9ce60010fa9408837d8f45b
dev1lfreak <kvachuk.sergey@yandex.ru>
2026-04-13 16:37:14 +0300

    fix : bug with assets

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/bg_forum/',
   plugins: [react({
     jsxRuntime: 'automatic',
   })],
