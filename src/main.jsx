2183e0467950b2689fc7a7cf9005dda97fa85584
dev1lfreak <kvachuk.sergey@yandex.ru>
2026-04-13 19:28:42 +0300

    fix : routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AppProvider } from './state/AppContext.jsx';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/bg_forum">
+  <HashRouter basename="/bg_forum">
     <AppProvider>
       <App />
     </AppProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 
